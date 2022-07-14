8d9e4a5c3fd5cc3f3c8192c5c341f7320bd848e5
s1290037 <s1290037@u-aizu.ac.jp>
2022-07-14 22:23:08 +0900

    HoT: initial revision

---
+++ HeadsORTails.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  int h=0, t=0;
+
+  printf("Tossing a coin...\n");
+
+  for(int i=1; i<=3; i++){
+    printf("Round %d: ", i);
+
+    if(rand()%2==1){ printf("Heads\n"); h++;}
+    else{ printf("Tails\n"); t++;}
+
+  }
+
+  printf("Heads: %d, Tails: %d\n", h, t);
+
+  return 0;
+  
+}
